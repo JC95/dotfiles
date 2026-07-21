67635d7d01b98fa792dc485c83c27d891cdc4196
Joe Cryer <joecryer@outlook.com>
2026-07-21 14:44:31 +0100

    change from hyprpolkitagent to polkit-gnome

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -35,7 +35,7 @@ local fileBrowser = "nautilus"
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function () 
   hl.exec_cmd("noctalia")
-  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
   hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
 end)
 
