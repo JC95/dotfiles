cc9d359cea1ad4914f0d335191a690253d0da11d
Joe Cryer <joecryer@outlook.com>
2026-07-27 19:59:44 +0100

    corrected treesitter for python, added basedpyright lsp

--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -3,7 +3,7 @@ local treesitter = require("nvim-treesitter")
 local ensure_installed = {
     "go", "rust", "typescript", "javascript", "tsx",
     "html", "css", "json", "bash",
-    "http", "dockerfile", "basedpyright",
+    "http", "dockerfile", "python",
 }
 
 treesitter.install(ensure_installed)
